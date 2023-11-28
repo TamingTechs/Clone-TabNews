d73c150006e6df17d6f2a661145be786816a901c
Taming Techs <tamingtechs+github@gmail.com>
2023-11-28 18:15:19 -0300

    created project files and installed project needs for development
    created .nvmrc .gitignore package.json package-lock.json
    
    local installed nvm node next react react-dom

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+	return <h1>Num relacionamento, Amor é saudável e Paixão é doença... No entanto, nos negócios e na profissão, sabemos
+que Amar problemas não é saudável. Mas temos que ter Paixão pelos mesmos, visto que proporcionam a geração de motivações que nos movem em direção às soluções.</h1>
+}
+
+export default Home
+
