6b9f7cf636587f88251742951c3d3e71f817b0d8
Taming Techs <126193663+TamingTechs@users.noreply.github.com>
2023-11-28 22:38:06 -0300

    Create index.html and nextjs.yml
    change from ubuntu-latest sid to debian-testing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+
 function Home() {
-	return <h1>Num relacionamento, Amor é saudável e Paixão é doença... No entanto, nos negócios e na profissão, sabemos
-que Amar problemas não é saudável. Mas temos que ter Paixão pelos mesmos, visto que proporcionam a geração de motivações que nos movem em direção às soluções.</h1>
+	const elementH1 = <h1>Mantenha isso em mente!</h1>;
+	const elementH3 = <h3>Paixão... Amor... Onde se encaixam nas nossas vidas?</h3>;
+	const elementP = <p>Num relacionamento, Amor é saudável e Paixão é doença... No entanto, nos negócios e na profissão, sabemos que Amar problemas não é saudável. Mas temos que ter Paixão pelos mesmos, visto que proporcionam a geração de motivações que nos movem em direção às soluções.</p>;
+	return <div>{[
+		elementH1,
+		elementH3,
+		elementP
+	]}</div>;
 }
 
 export default Home
-
